2b00c7c2e604c5c457f979520de0cd814d40df58
Hedzer <hedzer87@gmail.com>
2016-01-03 00:30:08 -0500

    Initial VS Files

---
+++ Urchin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urchin
+{
+	public class Urchin {
+
+	}
+    class KeySchedule {
+
+    }
+	class ProcessSchedule {
+
+	}
+}
