c46a69bd45cd584cd14978d3a5b3c66d33afe685
Hedzer <hedzer87@gmail.com>
2016-01-04 21:53:49 -0500

    Cipher, Layout

---
+++ Cipher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urchin {
+	public class Cipher {
+		public Urchin.Settings Settings = new Urchin.Settings();
+		public byte[] encrypt(byte[] plaintext) {
+			byte[] result = new byte[plaintext.Length];
+			return result;
+		}
+		public byte[] encrypt(String plaintext) {
+			byte[] result = new byte[plaintext.Length];
+			return result;
+		}
+		public byte[] decrypt(byte[] ciphertext) {
+			byte[] result = new byte[ciphertext.Length];
+			return result;
+		}
+		public byte[] decrypt(String ciphertext) {
+			byte[] result = new byte[ciphertext.Length];
+			return result;
+		}
+	}
+}
