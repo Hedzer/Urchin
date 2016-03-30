e6d76318654a57fc1578c62f3b3cff4e4520d185
Hedzer <hedzer87@gmail.com>
2016-03-30 12:19:54 -0400

    Processes, Added Not

---
+++ Processes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+
+namespace Urchin {
+	public static class Processes {
+		public delegate BitArray ProcessDelegate(string seed, BitArray data, bool inverse);
+		public static BitArray UnaryNot(string seed, BitArray data, bool inverse){
+			BitArray result = new BitArray(data);
+			result.Not();
+			return result;
+		}
+	}
+}
