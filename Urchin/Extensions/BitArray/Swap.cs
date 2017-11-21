46cb92620c0ec0b181d22fc0e036396763e58c29
Hedzer Ferwerda <hedzer87@gmail.com>
2017-11-21 16:34:30 -0500

    Added BitArray Swap extension method

---
+++ Urchin/Extensions/BitArray/Swap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Urchin.Extensions.BitArray
+{
+    static class SwapExt
+    {
+        public static void Swap(this System.Collections.BitArray instance, int indexA, int indexB)
+        {
+            bool current = instance.Get(indexA);
+            bool swap = instance.Get(indexB);
+            instance.Set(indexA, swap);
+            instance.Set(indexB, current);
+        }
+    }
+}
