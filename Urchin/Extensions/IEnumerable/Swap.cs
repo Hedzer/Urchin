81dfb4555f3224582163dc8580d2986951f69a6e
Hedzer Ferwerda <hedzer87@gmail.com>
2017-11-21 21:27:07 -0500

    Added Swap method to IEnum

---
+++ Urchin/Extensions/IEnumerable/Swap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urchin.Extensions.IEnumerable.Swap
+{
+    static class SwapExt
+    {
+        public static void Swap<T>(this T[] instance, int indexA, int indexB)
+        {
+            T current = instance[indexA];
+            T swap = instance[indexB];
+            instance[indexA] = swap;
+            instance[indexB] = current;
+        }
+    }
+}
