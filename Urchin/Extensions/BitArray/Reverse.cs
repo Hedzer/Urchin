a9467a72f9bcd0f7a533a481e0cfad4d2b2a4c91
Hedzer Ferwerda <hedzer87@gmail.com>
2017-11-21 21:05:27 -0500

    Added Reverse method to BitArray

---
+++ Urchin/Extensions/BitArray/Reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urchin.Extensions.BitArray.Reverse
+{
+    static class ReverseExt
+    {
+        public static void Reverse(this System.Collections.BitArray instance)
+        {
+            int length = instance.Length;
+            int mid = (length / 2);
+            for (int i = 0; i < mid; i++)
+            {
+                bool bit = instance[i];
+                instance[i] = instance[length - i - 1];
+                instance[length - i - 1] = bit;
+            }
+        }
+    }
+}
