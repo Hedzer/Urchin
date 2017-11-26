af7481495a59981b0ecb5c5a2f051a1dc49d43d5
Hedzer Ferwerda <hedzer87@gmail.com>
2017-11-26 13:43:46 -0500

    Added equality override

---
+++ Urchin/Extensions/BitArray/Equals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urchin.Extensions.BitArray.Equals
+{
+    public static class EqualsExt
+    {
+        public static bool Equals(this System.Collections.BitArray instance, System.Collections.BitArray bitArray)
+        {
+            int length = instance.Length;
+            if (bitArray == null) return false;
+            if (bitArray.Length != instance.Length) return false;
+            for (int i = 0; i < length; i++)
+            {
+                if (!Object.Equals(instance[i], bitArray[i])) return false;
+            }
+            return true;
+        }
+    }
+}
