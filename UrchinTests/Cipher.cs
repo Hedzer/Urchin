ae273df70655867c4155a9c29cfa030b5f80ec90
Hedzer Ferwerda <hedzer87@gmail.com>
2017-11-26 12:49:05 -0500

    Renamed to match project

---
+++ UrchinTests/Cipher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Urchin;
+
+
+
+
+namespace UrchinTests
+{
+    [TestClass]
+    public class Cipher
+    {
+        public static void Main()
+        {
+            
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Urchin.Cipher U = new Urchin.Cipher();
+        }
+    }
+}
