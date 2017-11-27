94ae0159f43cab5aa68f84a31437684a0754033d
Hedzer Ferwerda <hedzer87@gmail.com>
2017-11-27 09:34:20 -0500

    Replaced claculations with constants

--- Tests/Extensions/Words.cs
+++ Tests/Extensions/Words.cs
@@ -16,7 +16,7 @@ namespace Tests.Extensions
             HashAlgorithm hasher = new SHA512Managed();
             BitArray source = new BitArray(hasher.ComputeHash(new byte[] { 1, 2, 3, 4, 5 }));
             List<BitArray> words = source.ToWords(8);
-            Assert.AreEqual(source.Length / 8, words.Count);
+            Assert.AreEqual(64, words.Count);
         }
     }
 }
