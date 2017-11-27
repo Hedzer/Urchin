512e157f7ca7f888d34addfeb4799a5f665068ae
Hedzer Ferwerda <hedzer87@gmail.com>
2017-11-26 21:19:01 -0500

    Fixed issues caused by renaming Tests folder

--- Tests/Extensions/Words.cs
+++ Tests/Extensions/Words.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Urchin.Extensions.BitArray.Words;
 
 namespace Tests.Extensions
 {
-    [TestClass]
+    [TestClass, TestCategory("Extensions.BitArray.Words")]
     public class Words
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BitArrayToWords()
         {
+            HashAlgorithm hasher = new SHA512Managed();
+            BitArray source = new BitArray(hasher.ComputeHash(new byte[] { 1, 2, 3, 4, 5 }));
+            List<BitArray> words = source.ToWords(8);
+            Assert.AreEqual(source.Length / 8, words.Count);
         }
     }
 }
